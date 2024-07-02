148848443d7ae8a1a681ad3305a64ce078e88dc5
PraeThipwarin <prae.tippy@gmail.com>
2024-07-02 15:04:23 +0700

    q1 again

--- q1.js
+++ q1.js
@@ -1,24 +1,26 @@
-function typeChecker(a, b) {
-    if (typeof a === typeof b) {
-        if (typeof a === 'string') {
-            return "I LOVE YOU";
-        }
-        else if (typeof a === "number") {
-            return `${a+b}`;
-        }
+function typeChecker(a,b) {
+    if (typeof a === typeof b ) {
+      if (typeof a === 'string') {
+        return "I LOVE YOU";
+      }else if (typeof a === "number") {
+        return `${a+b}`;
+      }
+    }else{
+      return "NOT MATCHED";
     }
-    else {
-        return "NOT MATCHED";
-    }
-}
-//Test cases
-var a1 = "Jack";
-var b1 = "Rose";
-var a2 = 555;
-var b2 = 100;
-var a3 = "Jack";
-var b3 = 100;
-console.log(typeChecker(a1, b1));
-console.log(typeChecker(a2, b2));
-console.log(typeChecker(a3, b3));
-module.exports = typeChecker;
+  }
+  
+  //Test cases
+  const a1 = "Jack";
+  const b1 = "Rose";
+  const a2 = 555;
+  const b2 = 100;
+  const a3 = "Jack";
+  const b3 = 100;
+  
+  console.log(typeChecker(a1, b1));
+  console.log(typeChecker(a2, b2));
+  console.log(typeChecker(a3, b3));
+  
+  module.exports = typeChecker;
+  
